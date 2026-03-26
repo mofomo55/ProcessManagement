aa09b46daa4b49b8a72a7fe3d527e61f14bbc374
Mohamad Eibo <Mohamad Eibo@DESKTOP-1UQ8JGM>
2026-03-27 00:45:14 +0100

    DataBase connection + Project Strukture + Email Send Function

---
+++ ProcessManagment/ProcessManagment.Application/DTO/EmailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessManagment.Domain.Models
+{
+    public class EmailModel
+    {
+        public string? To { get; set; }
+        public string?  Subject { get; set; }
+        public string? Body { get; set; }
+    }
+}
---
+++ ProcessManagment/ProcessManagment.Domain/Models/BackgroundTasks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessManagment.Domain.Models
+{
+    public class BackgroundTasks
+    {
+        public int Id { get; set; }
+        public string? TaskType { get; set; }
+        public string? Data { get; set; }
+        public string? Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ProcessedAt { get; set; }
+
+
+    }
+}
